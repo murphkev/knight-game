0b7b0ef1a153005235862029581acd3cff7c4151
Kevin Murphy <kevscottmurphy@gmail.com>
2022-03-21 16:27:44 +1100

    changed help button to api call and updated css

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -36,88 +36,13 @@ export default class Game extends React.Component {
             this.postMessage("Please start a new game");
         }
         else {
-            let path = this.getShortestPath();
-            this.moveKnight(path.shift());
+            fetch(`https://glacial-savannah-02062.herokuapp.com/knight?x=${this.state.knight[0]}&y=${this.state.knight[1]}&tx=${this.state.goal[0]}&ty=${this.state.goal[1]}`, 
+                { method: 'GET' })
+                .then(res => res.json())
+                .then(data => {
+                        this.moveKnight(data.path.shift());
+                });           
         }
-    }
-
-    // gets the shortest path to the goal
-    // mostly learnt /adapted from https://www.geeksforgeeks.org/minimum-steps-reach-target-knight/
-    getShortestPath() {
-        // for brevity's sake
-        let pos = this.state.knight;
-        let target = this.state.goal;
-
-        // all possible moves relative to current pos
-        let deltaX = [-2, -1,  1,  2, -2, -1, 1, 2];
-        let deltaY = [-1, -2, -2, -1,  1,  2, 2, 1];
-
-        // queue of moves including move history
-        let queue = [];
-        queue.push([pos, []]);
-
-        // boolean version of chess board to check visited squares
-        // so that we don't revisit the same square and end up in
-        // an infinite loop
-        let visited = new Array(8);
-        for(let i = 0; i < visited.length; ++i) {
-            visited[i] = new Array(8);
-            for(let j = 0; j < visited[i].length; ++j) {
-                visited[i][j] = false;
-            }
-        }
-
-        // mark the current knight pos as visited
-        visited[pos[0]][pos[1]] = true;
-
-        // vars for the new position
-        let nPos, x, y;
-
-        // loop until there is one element in queue
-        while (queue.length !== 0) {
-
-            // get the first pos
-            /* 
-                NB: because positions are added in order of 
-                lowest to greatest 'distance' to target,
-                we don't need to sort the array, or compare the 
-                distance of moves; the first element will always
-                have the lowest, or equal lowest move count.
-            */
-            nPos = queue.shift();
-
-            // if that's the target...
-            if (this.isSameSquare(nPos[0], target)) {
-                return nPos[1];
-            }
-
-            // otherwise check every other move possible
-            for (let i = 0; i < 8; ++i) {
-                x = nPos[0][0] + deltaX[i];
-                y = nPos[0][1] + deltaY[i];
-
-                // make sure those squares are valid, and not previously visited
-                if (this.isValidSquare([x, y]) && !visited[x][y]) {
-                    // mark square as visited
-                    visited[x][y] = true;
-                    let history = [...nPos[1], [x, y]];
-                    queue.push([[x, y], history]);
-                }
-            }
-        }
-
-        // unsolvable, which should be impossible
-        return [];
-    }
-
-    // checks to see if a square is valid for the board
-    isValidSquare(pos) {
-        return pos && 
-            Array.isArray(pos) && 
-            pos[0] >= 0 && 
-            pos[0] <= 7 && 
-            pos[1] >= 0 && 
-            pos[1] <= 7;
     }
 
     // draws the game UI
